b139ce8a16843bedb7245b7060993d8babf1a4f0
Artyom Pak <44568508+artyomvp1@users.noreply.github.com>
2021-02-25 21:50:50 -0800

    Procedure + Job for dublicate cleaning

--- Procedures_SQL/FEB092021_sdp_dublicate_o.sql
+++ Procedures_SQL/FEB092021_sdp_dublicate_o.sql
@@ -2,22 +2,29 @@ CREATE OR REPLACE PROCEDURE sdp_dublicate_o
 AS
     
 BEGIN
-    EXECUTE IMMEDIATE 'TRUNCATE TABLE daily_cache' ; -- Don't forget
+    EXECUTE IMMEDIATE 'TRUNCATE TABLE daily_cache' ;
     
 
     DELETE
     FROM spd2@SDP
-    WHERE transaction_date >= &start_date
+    WHERE transaction_date >= TRUNC(SYSDATE)
       AND ROWID NOT IN ( SELECT MIN(ROWID)
                          FROM spd2@SDP
-                         GROUP BY transaction_id,
-                                  transaction_date, 
-                                  tran_type,
-                                  sdp_mode, 
-                                  number_of_rows,
-                                  cache_state,
-                                  commentary
+                         GROUP BY transaction_id, transaction_date, tran_type, sdp_mode, 
+                                  number_of_rows, cache_state, commentary
                         ) ;
                         
     COMMIT ;
+END ;
+/
+
+BEGIN
+    DBMS_SCHEDULER.CREATE_JOB (job_name             => 'clean_dublicate_sdp',
+                               job_type             => 'STORED_PROCEDURE',
+                               job_action           => 'sdp_dublicate_o',
+                               start_date           => SYSTIMESTAMP,
+                               repeat_interval      => 'freq=daily; interval=1;', -- on daily basis
+                               end_date             => NULL,
+                               enabled              => TRUE,
+                               comments             => 'Delete dublicate rows from the cache. Table: daily_cache') ;
 END ;
